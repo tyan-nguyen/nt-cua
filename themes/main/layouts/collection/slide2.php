55b3713fd9f1dda382583abe0b8e84b2966a28e2
tyan-nguyen <nguyenvantyan@gmail.com>
2023-08-22 13:54:53 +0700

    update

---
+++ themes/main/layouts/collection/slide2.php
@@ -0,0 +1,32 @@
+<div class="swiper-slide overflow-hidden">
+<div class="product-card">
+  <div class="card-detail d-flex justify-content-between align-items-baseline pt-3">
+    <h3 class="card-title text-uppercase">
+      <a href="shop.html">HỆ CỬA SỔ</a>
+    </h3>
+  </div>
+  <div class="image-overlay position-relative">
+    <div class="product-image">
+      <img src="<?= Yii::getAlias('@web')  ?>/images/product-item5.jpg" alt="product-item" class="product-image img-fluid">
+      <div class="text-box box-slide position-absolute">
+        <div class="text-content p-5 bg-light">
+          <h3>Hệ cửa sổ</h3>
+          <p>Chúng tôi cung cấp các loại cửa sổ đa dạng về hình thức và chủng loại sử dụng nhôm Xingfa hệ 55.</p>
+          <ul>
+            <li>Cửa sổ 1 cánh</li>
+            <li>Cửa sổ 1 cánh - Lam sáng</li>
+            <li>Cửa sổ 2 cánh</li>
+            <li>Cửa sổ 2 cánh - Lam sáng</li>
+          </ul>
+          <a href="shop.html" class="btn btn-medium btn-arrow position-relative mt-5">
+                        <span class="text-uppercase">Xem sản phẩm</span>            
+                        <svg class="arrow-right position-absolute" width="18" height="20">
+                          <use xlink:href="#arrow-right"></use>
+                        </svg>
+                      </a>   
+        </div>
+      </div>
+    </div>
+  </div>                  
+</div>
+</div>
---
+++ themes/main/layouts/collection/slide3.php
@@ -0,0 +1,29 @@
+<div class="swiper-slide overflow-hidden">
+<div class="product-card">
+  <div class="card-detail d-flex justify-content-between align-items-baseline pt-3">
+    <h3 class="card-title text-uppercase">
+      <a href="shop.html">CỬA THỦY LỰC</a>
+    </h3>
+  </div>
+  <div class="image-overlay position-relative">
+    <div class="product-image">
+      <img src="<?= Yii::getAlias('@web')  ?>/images/product-item7.jpg" alt="product-item" class="product-image img-fluid">
+
+      <div class="text-box box-slide position-absolute">
+        <div class="text-content p-5 bg-light">
+          <h3>Cửa thủy lực</h3>
+          <p>Cửa kính thủy lực là sự kết hợp hoàn hảo giữa kính cường lực với hệ thống pittong thủy lực âm sàn (bản lề thủy lực). Bản chất của tên gọi cửa thủy lực đó là do bản lề cửa kính thủy lực có cấu tạo, thiết kế khác với bản lề các loại cửa gỗ, cửa sắt…Bản lề được đặt nằm ngầm dưới sàn và có sự hỗ trợ của pittong thủy lực. Cho phép cửa tự động quay về vị trí cũ một cách nhẹ nhàng sau khi mở.</p>
+         
+          <a href="shop.html" class="btn btn-medium btn-arrow position-relative mt-5">
+                        <span class="text-uppercase">Xem sản phẩm</span>            
+                        <svg class="arrow-right position-absolute" width="18" height="20">
+                          <use xlink:href="#arrow-right"></use>
+                        </svg>
+                      </a>   
+        </div>
+      </div>
+    </div>
+  <!-- product-image -->
+  </div>                  
+</div>
+</div>
