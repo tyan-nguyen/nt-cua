5086bcf785da56fbd740eb53d8a815e137a4de1c
tyan-nguyen <nguyenvantyan@gmail.com>
2023-08-26 16:07:26 +0700

    update

--- themes/main/layouts/head.php
+++ themes/main/layouts/head.php
@@ -10,8 +10,8 @@
     <meta name="keywords" content="">
     <meta name="description" content="">
     <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="/css/style.css?v=7">
-    <link rel="stylesheet" type="text/css" href="/css/custom.css?v=8">
+    <link rel="stylesheet" type="text/css" href="/css/style.css?v=9">
+    <link rel="stylesheet" type="text/css" href="/css/custom.css?v=9">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
