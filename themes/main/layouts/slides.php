30145e903b41105dc944ed04eabfffa0f015befe
tyan-nguyen <nguyenvantyan@gmail.com>
2023-09-23 08:39:10 +0700

    update

--- themes/main/layouts/slides.php
+++ themes/main/layouts/slides.php
@@ -11,8 +11,8 @@
                   	
                   	<!-- <video controls autoplay playsinline width="100%"> -->
                   	<video controls autoplay loop playsinline width="100%">
-                      <source src="<?= Yii::getAlias('@web')  ?>/img/video.mp4?v=1" type="video/mp4"> 
-                      <!--<source src="<?= Yii::getAlias('@web')  ?>/img/video.mov?v=1" type="video/mp4"> -->
+                      <!-- <source src="<?= Yii::getAlias('@web')  ?>/img/video.mp4?v=1" type="video/mp4">  -->
+                     <source src="<?= Yii::getAlias('@web')  ?>/img/video.mov?v=1" type="video/mp4">
                       <!-- <source src="movie.ogg" type="video/ogg"> -->
                       Your browser does not support the video tag.
                     </video> 
